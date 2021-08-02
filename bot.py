5d2feb9a2405f0c50382ac6d342580b9186501cb
eltaylor1104 <eltaylor1104@gmail.com>
2021-08-02 13:02:14 -0700

    ctx

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def on_ready():
 	print("Bot is online.")
 
 @bot.command()
-async def ping(self, ctx): # Defines a new "context" (ctx) command called "ping."
+async def ping(ctx): # Defines a new "context" (ctx) command called "ping."
 	await ctx.send(f"Pong! 🏓 ({bot.latency*1000}ms)")
 
 @bot.command(name="update")
