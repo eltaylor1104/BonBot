d981254135e2d765c1ae286e8f947d3ea9e9e017
eltaylor1104 <eltaylor1104@gmail.com>
2021-08-02 15:16:10 -0700

    fixes

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,57 +1,51 @@
 from discord.ext import commands
-from dislash import *
-from dotenv import load_dotenv
-import discord
-import os
-import dislash
-import jishaku
-
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix="s!")
-slash = SlashClient(bot)
-test_ids = [804935799316676629] # Put your server ID in this array
 
 
 class owner(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
-
-
-    @commands.command(hidden=True)
+    
+    # Hidden means it won't show up on the default help.
+    @commands.command(name='load', hidden=True)
     @commands.is_owner()
-    async def load(self, *, module : str):
-        """Loads a module."""
-        try:
-            self.bot.load_extension(module)
-        except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
-        else:
-            await self.bot.say('\N{OK HAND SIGN}')
+    async def cog_load(self, ctx, *, cog: str):
+        """Command which Loads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
 
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def unload(self, *, module : str):
-        """Unloads a module."""
         try:
-            self.bot.unload_extension(module)
+            self.bot.load_extension(cog)
         except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
-            await self.bot.say('\N{OK HAND SIGN}')
+            await ctx.send('**`SUCCESS`**')
+
+    @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
+    async def cog_unload(self, ctx, *, cog: str):
+        """Command which Unloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.unload_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
 
     @commands.command(name='reload', hidden=True)
     @commands.is_owner()
-    async def _reload(self, *, module : str):
-        """Reloads a module."""
+    async def cog_reload(self, ctx, *, cog: str):
+        """Command which Reloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
         try:
-            self.bot.unload_extension(module)
-            self.bot.load_extension(module)
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
         except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
-            await self.bot.say('\N{OK HAND SIGN}')
+            await ctx.send('**`SUCCESS`**')
 
 
 def setup(bot):
