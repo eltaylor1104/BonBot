1f686ff563557b848695fb9f564ac514606b804c
eltaylor1104 <eltaylor1104@gmail.com>
2021-08-02 19:57:15 -0700

    remove regular cmds

--- cogs/owner.py
+++ cogs/owner.py
@@ -15,46 +15,7 @@ class owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    # Hidden means it won't show up on the default help.
-    @commands.command(name='load', hidden=True)
-    @commands.is_owner()
-    async def load(self, ctx, *, cog: str):
-        """Command which Loads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
 
-        try:
-            self.bot.load_extension(f'cogs.{cog}')
-        except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-        else:
-            await ctx.send(f'📥**`{cog}`**')
-
-    @commands.command(name='unload', hidden=True)
-    @commands.is_owner()
-    async def unload(self, ctx, *, cog: str):
-        """Command which Unloads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
-
-        try:
-            self.bot.unload_extension(f'cogs.{cog}')
-        except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-        else:
-            await ctx.send(f'📤**`{cog}`**')
-
-    @commands.command(name='reload', hidden=True)
-    @commands.is_owner()
-    async def reload(self, ctx, *, cog: str):
-        """Command which Reloads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
-
-        try:
-            self.bot.unload_extension(f'cogs.{cog}')
-            self.bot.load_extension(f'cogs.{cog}')
-        except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-        else:
-            await ctx.send(f'🔁**`{cog}`**')
 
     @slash_commands.command(name='load', guild_ids=test_ids, description="owner only", options=[Option("cog", "a cog to load", Type.STRING, required=True)])
     @slash_commands.is_owner()
