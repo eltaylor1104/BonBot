d2fc6a1ba06de1c60b2aab32dc3c1def098bdfcf
eltaylor1104 <eltaylor1104@gmail.com>
2021-08-02 18:57:39 -0700

    xD

--- cogs/owner.py
+++ cogs/owner.py
@@ -18,7 +18,7 @@ class owner(commands.Cog):
         except Exception as e:
             await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send(f'📥**`{cog}`**')
 
     @commands.command(name='unload', hidden=True)
     @commands.is_owner()
@@ -31,7 +31,7 @@ class owner(commands.Cog):
         except Exception as e:
             await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send(f'📤**`{cog}`**')
 
     @commands.command(name='reload', hidden=True)
     @commands.is_owner()
@@ -45,7 +45,7 @@ class owner(commands.Cog):
         except Exception as e:
             await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send(f'🔁**`{cog}`**')
 
 
 
