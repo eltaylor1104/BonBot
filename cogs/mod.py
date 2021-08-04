d639e7edc8cf18eee3721e9cb31dccceb90154c0
eltaylor1104 <eltaylor1104@gmail.com>
2021-08-03 22:57:32 -0700

    make addrole global, add global removerole cmd

--- cogs/mod.py
+++ cogs/mod.py
@@ -48,13 +48,21 @@ class mod(commands.Cog):
             await ctx.channel.purge(limit=amount)
             await ctx.send(f'Cleared {amount} messages.', ephemeral=True)
 
-    @slash_commands.command(name='addrole', description='Add a role to a specified user', guild_ids=test_ids, options=[Option("user", "a user to add a role to", Type.USER, required=True),
+    @slash_commands.command(name='addrole', description='Add a role to a specified user', options=[Option("user", "a user to add a role to", Type.USER, required=True),
     Option("role", "a role to add", Type.ROLE, required=True)])
     @slash_commands.has_permissions(manage_roles=True)
     @slash_commands.guild_only()
     async def addrole(self, ctx, user, role):
         await user.add_roles(role)
         await ctx.send(f'{role} was added to {user}.', ephemeral=True)
+
+    @slash_commands.command(name='removerole', description='Remove a role from a specified user', options=[Option("user", "a user to remove a role from", Type.USER, required=True),
+    Option("role", "a role to remove", Type.ROLE, required=True)])
+    @slash_commands.has_permissions(manage_roles=True)
+    @slash_commands.guild_only()
+    async def addrole(self, ctx, user, role):
+        await user.remove_roles(role)
+        await ctx.send(f'{role} was removed from {user}.', ephemeral=True)
 
 
 
