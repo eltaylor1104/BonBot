7563c6af6c8351886cda292cb90afb035fedad0c
eltaylor1104 <eltaylor1104@gmail.com>
2021-09-12 19:07:32 +0000

    add file to store bot emojis

---
+++ constants.py
@@ -0,0 +1,8 @@
+import dataclasses
+
+@dataclasses.dataclass
+class Emojis:
+    upvote= "<:stack_upvote:886680335914532925>"
+    eye = "<:stack_eye:886678683572650105>"
+    tag = "<:stack_tag:886678769417478155>"
+    comments = "<:stack_comments:886680106234425374>"
